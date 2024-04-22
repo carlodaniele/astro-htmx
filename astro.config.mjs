5fb6c086e31c2c104db81e07c5df78e1ad31e651
carlodaniele <carlo.daniele@gmail.com>
2024-04-22 19:26:13 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,12 @@ import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
   integrations: [db()],
+  site: 'https://example.com',
   output: "server",
   adapter: node({
     mode: "standalone"
   }),
-	server: {
-		host: true
-	}
+  server: {
+    host: true
+  }
 });
