7e989d2cffda177bdb807b721add27b1cd8a022d
carlodaniele <carlo.daniele@gmail.com>
2024-04-22 21:25:56 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
 import db from "@astrojs/db";
-
 import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [db()],
-  site: 'https://example.com',
+  site: 'https://astro-htmx-r063g.kinsta.app/',
   output: "server",
   adapter: node({
     mode: "standalone"
